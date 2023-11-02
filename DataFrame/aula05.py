ad0f1c11ceedf23b0b6c8632736d0ca03c509298
pachecosamuel <pacheco.samuel@hotmail.com>
2023-11-02 09:46:17 -0300

    More about dataframe

--- DataFrame/aula05.py
+++ DataFrame/aula05.py
@@ -1,18 +1,33 @@
 import pandas as pd
 
 df_vendas = pd.read_excel("Vendas_Jan.xlsx", dtype=str, engine='openpyxl')
-print(df_vendas)
 
-# 1° - Como dispar algumas colunas
+# 1° - Show some specific columns
+print(f"\n Showing some specific columns:\n")
+print(f"{df_vendas[['Vendedor', 'Total Vendas']]}\n")
 
-# 2° - Dispar linhas de 1 ao 5
+# 2° - Showings rows from one to five
+print(f"\n Showings rows from one to five:\n")
+print(f"{df_vendas.loc[1:5]}\n")
 
-# 3° - Imprimir vendas Leonardo
+# 3° - Showing only Leonardo sales
+print(f"\n Showing only Leonardo sales:\n")
+df_leo_sales = df_vendas.loc[df_vendas["Vendedor"] == "Leonardo Almeida"]
+print(f"{df_leo_sales}\n")
 
-# 4° - Imprimar as vendas e resumir infos
+# 4° - Print sales and resume the informations
+print(f"\n Print sales and resume the informations:\n")
+print(f"{df_leo_sales[['Vendedor', 'Total Vendas']]}")
 
-# 5° - Imprimir um único índice e coluna
+# 5° - Print a unique index and columns
+print(f"\n Print a unique index and columns:")
+print(f"{df_vendas.loc[27, 'Vendedor']}\n")
 
-# 6° - Usando o método shape
+# 6° - Using the shape method = Amout of rows and columns
+print(f"\n Using the shape method:")
+print(f"{df_vendas.shape}\n")
 
-# 7° - Usando o método transpose
+# 7° - Using transpose method = Transform rows in columns and columns in rows
+print(f"\n Using the transpose method:")
+print(f"{df_vendas.T}\n")
+
